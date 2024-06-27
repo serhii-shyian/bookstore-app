e7855619056fdb48d878207fc8c5fb797714b03d
Serhii Shyian <serhii.shn@gmail.com>
2024-06-27 11:49:16 +0300

    added basic-structure

--- src/main/java/org/example/bookstore/BookstoreApplication.java
+++ src/main/java/org/example/bookstore/BookstoreApplication.java
@@ -1,13 +1,34 @@
 package org.example.bookstore;
 
+import java.math.BigDecimal;
+import lombok.RequiredArgsConstructor;
+import org.example.bookstore.model.Book;
+import org.example.bookstore.service.BookService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@RequiredArgsConstructor
 public class BookstoreApplication {
+    private final BookService bookService;
 
     public static void main(String[] args) {
         SpringApplication.run(BookstoreApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner startup() {
+        return args -> {
+            Book book = new Book(
+                    "Camino Ghosts",
+                    "John Grisham",
+                    "0385545991",
+                    BigDecimal.valueOf(22.20));
+            bookService.save(book);
+            System.out.println(bookService.findAll());
+        };
     }
 
 }
---
+++ src/main/java/org/example/bookstore/exception/DataProcessingException.java
@@ -0,0 +1,7 @@
+package org.example.bookstore.exception;
+
+public class DataProcessingException extends RuntimeException {
+    public DataProcessingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
